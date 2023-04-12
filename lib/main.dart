607391bfa7452267097f1eddc7a957c7bdf79b49
bamboo-wood <cantate.1009.0417@gmail.com>
2023-04-12 13:01:12 +0900

    generate count data model

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod_explanation/Provider/provider.dart';
+import 'package:flutter_riverpod_explanation/providers/provider.dart';
 
 void main() {
   runApp(
